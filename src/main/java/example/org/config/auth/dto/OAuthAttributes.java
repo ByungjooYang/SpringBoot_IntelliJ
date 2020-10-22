4443e8b4f549cf86222da9fbb20810c0e787a8fa
ByungjooYang <byungjoo104@gmail.com>
2020-10-22 21:27:09 +0900

    네이버 로그인 시 이름 문제 해결

--- src/main/java/example/org/config/auth/dto/OAuthAttributes.java
+++ src/main/java/example/org/config/auth/dto/OAuthAttributes.java
@@ -33,17 +33,24 @@ public class OAuthAttributes {
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+
         return OAuthAttributes.builder().name((String)attributes.get("name"))
                                         .email((String)attributes.get("email"))
                                         .picture((String)attributes.get("picture"))
-                                        .attributes(attributes).nameAttributeKey(userNameAttributeName).build();
+                                        .attributes(attributes)
+                                        .nameAttributeKey(userNameAttributeName)
+                                        .build();
     }
 
     private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
         Map<String, Object> response = (Map<String, Object>)attributes.get("response");
 
-        return OAuthAttributes.builder().name((String) response.get("name")).email((String) response.get("email")).picture((String) response.get("profile_image")).attributes(response)
-                .nameAttributeKey(userNameAttributeName).build();
+        return OAuthAttributes.builder().name((String) response.get("name"))
+                                        .email((String) response.get("email"))
+                                        .picture((String) response.get("profile_image"))
+                                        .attributes(response)
+                                        .nameAttributeKey(userNameAttributeName)
+                                        .build();
     }
 
     public User toEntity() {
