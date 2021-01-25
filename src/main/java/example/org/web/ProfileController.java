8c80f075fac972e5759ef2062fab6b8ea7e87890
ByungjooYang <byungjoo104@gmail.com>
2021-01-25 22:56:41 +0900

    엔진엑스 기본설정

---
+++ src/main/java/example/org/web/ProfileController.java
@@ -0,0 +1,31 @@
+package example.org.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class ProfileController {
+    private final Environment env;
+
+    @GetMapping("/profile")
+    public String profile() {
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("DB", "DB1", "DB2");
+
+        String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
+
+        return profiles.stream().filter(realProfiles::contains).findAny().orElse(defaultProfile);
+    }
+}
+
+/*
+env.getActiveProfiles()
+ : 현재 실행 중인 ActiveProfile을 모두 가져온다. 즉, oauth, DB, AWS 등이 활성화 되어 있다면 3개가 모두 담겨있다.
+  여기서 AWS, AWS1, AWS2 는 모두 매포에 사용될 profile 이라 이 중 하나만 있으면 그 값을 반환하도록 한다.
+ */
---
+++ src/test/java/example/org/web/ProfileControllerUnitTest.java
@@ -0,0 +1,65 @@
+package example.org.web;
+
+import org.junit.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import javax.persistence.GeneratedValue;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProfileControllerUnitTest {
+    @Test
+    public void find_DB_profile() {
+        //given
+        String expectedProfile = "DB";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("AWS");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+
+    }
+
+    @Test
+    public void DB_profile_else_first() {
+        //given
+        String expectedProfile = "oauth";
+        MockEnvironment env = new MockEnvironment();
+
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("AWS");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+
+    }
+
+    @Test
+    public void DB_profile_else_second(){
+        //given
+        String expectedProfile = "AWS";
+        MockEnvironment env = new MockEnvironment();
+
+        env.addActiveProfile(expectedProfile);
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+}
