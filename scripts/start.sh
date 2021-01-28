ca1d6a9b3163a317a02f20b4b8fbf68a56d50f8b
ByungjooYang <byungjoo104@gmail.com>
2021-01-28 16:19:32 +0900

    오타 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,7 +10,7 @@ PROJECT_NAME=SpringBoot_IntelliJ
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
